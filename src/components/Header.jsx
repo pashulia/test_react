2f4385d209052c1a6a9f6a04de6941e1cf8480bd
Pavel Kho <pashuliia.pk@gmail.com>
2020-12-22 13:30:38 +0600

    this is my first project written in react

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className='header'>
+      <img src="https://logomaster.ai/static/media/sample.c8bf2b04.svg" />
+    </header>
+  )
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+      <div className='content'>
+        <div> 
+          <img className="headimg" src="https://wowslider.com/sliders/demo-18/data1/images/hongkong1081704.jpg" />
+        </div>
+        <div>
+          ava+description
+        </div>
+        <div>
+          My post
+          <div>
+            New post
+          </div>
+          <div>
+            <div>
+              post 1
+            </div>
+            <div>
+              post 2
+            </div>
+          </div>
+        </div>
+      </div>
+  )
+}
+
+export default Profile;
